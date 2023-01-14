99e04798ba66ef51a8ec640e6f5fea6184f32f19
Eli Barlow <exbarlow@gmail.com>
2023-01-14 12:51:19 -0600

    G18

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import math
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -15,19 +17,34 @@ p.loadSDF("world.sdf")
 
 ## Prepare robot simulation
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
+#!!!!!! G7 should be `robotID` not `robot`, as we were previously told to store the id in `robotId
 
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex= robotId,
+        jointName = b'Torso_BackLeg',
+        controlMode= p.POSITION_CONTROL,
+        targetPosition= (random.uniform(-math.pi/2,math.pi/2)),
+        maxForce= 50
+    )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex= robotId,
+        jointName = b'Torso_FrontLeg',
+        controlMode= p.POSITION_CONTROL,
+        targetPosition= (random.uniform(-math.pi/2,math.pi/2)),
+        maxForce= 50
+    )
     time.sleep(1/100)
+
 
 np.save("data/backLegSensorValues.npy",backLegSensorValues)
 np.save("data/frontLegSensorValues.npy",frontLegSensorValues)
-print(backLegSensorValues)
 
 p.disconnect()
 
