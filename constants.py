1007a5f70b17735325baa1d3261a28f56a476d99
Eli Barlow <exbarlow@gmail.com>
2023-01-14 13:41:21 -0600

    H9

---
+++ constants.py
@@ -0,0 +1,15 @@
+import math
+
+gravity = (0,0,-9.8)
+timeSteps = 1000
+sleepTime = 1/800
+maxForce = 250
+
+amplitude_bl = math.pi/4
+frequency_bl = 5
+phaseOffset_bl = 0
+
+amplitude_fl = math.pi/4
+frequency_fl = 5
+phaseOffset_fl = math.pi/2
+
--- simulate.py
+++ simulate.py
@@ -5,34 +5,28 @@ import pyrosim.pyrosim as pyrosim
 import numpy as np
 import math
 import random
+import constants as c
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 ## Set up world
-p.setGravity(0,0,-9.8)
+p.setGravity(*c.gravity)
+
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 ## Prepare robot simulation
 pyrosim.Prepare_To_Simulate(robotId)
-timeSteps = 1000
-backLegSensorValues = np.zeros(timeSteps)
-frontLegSensorValues = np.zeros(timeSteps)
+backLegSensorValues = np.zeros(c.timeSteps)
+frontLegSensorValues = np.zeros(c.timeSteps)
 
-amplitude_bl = math.pi/4
-frequency_bl = 5
-phaseOffset_bl = 0
+targetAngles = np.linspace(0,2*math.pi,c.timeSteps)
 
-amplitude_fl = math.pi/4
-frequency_fl = 5
-phaseOffset_fl = math.pi/2
-
-targetAngles = np.linspace(0,2*math.pi,timeSteps)
-
-targetAngles_bl = [amplitude_bl * np.sin(frequency_bl * i + phaseOffset_bl) for i in targetAngles]
-targetAngles_fl = [amplitude_fl * np.sin(frequency_fl * i + phaseOffset_fl) for i in targetAngles]
+targetAngles_bl = [c.amplitude_bl * np.sin(c.frequency_bl * i + c.phaseOffset_bl) for i in targetAngles]
+targetAngles_fl = [c.amplitude_fl * np.sin(c.frequency_fl * i + c.phaseOffset_fl) for i in targetAngles]
 
 # np.save("data/targetAngles_bl.npy",targetAngles_bl)
 # np.save("data/targetAngles_fl.npy",targetAngles_fl)
@@ -40,7 +34,7 @@ targetAngles_fl = [amplitude_fl * np.sin(frequency_fl * i + phaseOffset_fl) for 
 
 #!!!!!! G7 should be `robotID` not `robot`, as we were previously told to store the id in `robotId
 
-for i in range(timeSteps):
+for i in range(c.timeSteps):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -49,16 +43,16 @@ for i in range(timeSteps):
         jointName = b'Torso_BackLeg',
         controlMode= p.POSITION_CONTROL,
         targetPosition= targetAngles_bl[i],
-        maxForce= 250
+        maxForce= c.maxForce
     )
     pyrosim.Set_Motor_For_Joint(
         bodyIndex= robotId,
         jointName = b'Torso_FrontLeg',
         controlMode= p.POSITION_CONTROL,
         targetPosition= targetAngles_fl[i],
-        maxForce= 250
+        maxForce= c.maxForce
     )
-    time.sleep(1/800)
+    time.sleep(c.sleepTime)
 
 
 np.save("data/backLegSensorValues.npy",backLegSensorValues)
