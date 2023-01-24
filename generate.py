b8b5d8a54fa34cdb238a67ff25509f16958b7cca
Eli Barlow <exbarlow@gmail.com>
2023-01-24 12:08:37 -0600

    L33

--- generate.py
+++ generate.py
@@ -1,14 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 import random
 
-
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
 
     pyrosim.Send_Cube(name="Box",pos=[3,3,0.5],size=[1,1,1])
 
     pyrosim.End()
-
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
--- hillclimber.py
+++ hillclimber.py
@@ -4,4 +4,6 @@ from solution import SOLUTION
 class HILL_CLIMBER:
     def __init__(self):
         self.parent = SOLUTION()
-        pass
+    
+    def Evolve(self):
+        self.parent.Evaluate()
